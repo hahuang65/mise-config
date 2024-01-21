c6b3b7e5be4cd23f70a8070c7adb4ff84b9e38b9
Howard Huang <hao@hwrd.me>
2024-01-21 15:54:44 -0600

    Add files for default language packages to be installed

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ln -sf "${PWD}/default-python-packages" "${HOME}/.default-python-packages"
+ln -sf "${PWD}/default-npm-packages" "${HOME}/.default-npm-packages"
+ln -sf "${PWD}/default-ruby-gems" "${HOME}/.default-gems"
