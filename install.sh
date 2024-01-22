e3735334939360ea5800a670c57fc8ddd97700d4
Howard Huang <hao@hwrd.me>
2024-01-21 22:49:19 -0600

    Add default-go-packages

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 ln -sf "${PWD}/default-python-packages" "${HOME}/.default-python-packages"
 ln -sf "${PWD}/default-npm-packages" "${HOME}/.default-npm-packages"
 ln -sf "${PWD}/default-ruby-gems" "${HOME}/.default-gems"
+ln -sf "${PWD}/default-go-packages" "${HOME}/.default-go-packages"
