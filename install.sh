5df0e2a6192608e9c9970e05f54991e035d0ac88
Howard Huang <hao@hwrd.me>
2024-02-09 21:41:10 -0600

    Add a shim for bridging asdf and mise on macOS for Summit work

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ ln -sf "${PWD}/default-python-packages" "${HOME}/.default-python-packages"
 ln -sf "${PWD}/default-npm-packages" "${HOME}/.default-npm-packages"
 ln -sf "${PWD}/default-ruby-gems" "${HOME}/.default-gems"
 ln -sf "${PWD}/default-go-packages" "${HOME}/.default-go-packages"
+
+if [ $(uname) = "Darwin" ]; then
+  ln -sf "${PWD}/asdf" "${HOME}/.local/bin/asdf"
+fi
