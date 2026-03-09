610e1388444229ff56697427e8773645e005d2d8
Howard Huang <hao@hwrd.me>
2026-03-09 14:42:04 -0500

    Add rails and a config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf "${PWD}/default-python-packages" "${HOME}/.default-python-packages"
 ln -sf "${PWD}/default-npm-packages" "${HOME}/.default-npm-packages"
 ln -sf "${PWD}/default-ruby-gems" "${HOME}/.default-gems"
 ln -sf "${PWD}/default-go-packages" "${HOME}/.default-go-packages"
+ln -sf "${PWD}/railsrc" "${HOME}/.railsrc"
 
 if [ "$(uname)" = "Darwin" ]; then
   ln -sf "${PWD}/asdf" "${HOME}/.local/bin/asdf"
